902718730f85cf461ff39d3d4b2fb53e964074d3
Ayarn <iamayarn2001@gmail.com>
2023-09-17 22:06:42 +0530

    Solution for Task-2

---
+++ Task2.js
@@ -0,0 +1,9 @@
+const input = "madam";
+
+const reverseOfInput = input.split('').reverse().join('');
+
+if (input === reverseOfInput) {
+    console.log("true");
+} else {
+    console.log("false");
+}
